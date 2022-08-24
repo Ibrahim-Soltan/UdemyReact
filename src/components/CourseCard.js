b106c10d91c933ed3b378359aef889a0e8a32b75
Ibrahim Soltan <ibrahim.soltan918@gmail.com>
2022-08-24 16:11:09 +0200

    chore: Creating a styles dir.

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faStar} from "@fortawesome/free-solid-svg-icons"
 import img from "../assets/python/course1.jpg"
-import CourseCardCSS from "./CourseCard.module.css"
+import CourseCardCSS from "./styles/CourseCard.module.css"
 
 function CourseCard(props) {
 
