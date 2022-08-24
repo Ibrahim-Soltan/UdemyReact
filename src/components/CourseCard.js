ab754ecaa4c9583060bfea3bf3e2c621e2d72d75
Ibrahim Soltan <ibrahim.soltan918@gmail.com>
2022-08-24 11:21:41 +0200

    feat: Styling CourseCard Component.
    Styling CourseCard Component using CourseCard.module.css

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -2,27 +2,25 @@ import React from 'react'
 import { FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faStar} from "@fortawesome/free-solid-svg-icons"
 import img from "../assets/python/course1.jpg"
+import CourseCardCSS from "./CourseCard.module.css"
 
 function CourseCard(props) {
   return (
-    <div className="courses">
-        <div className="course">
+        <div className={CourseCardCSS.course}>
         <img src={img} alt="course banner" />
-        <h4 className="courseName">
+        <h4 className={CourseCardCSS.courseName}>
             {props.name}
         </h4>
-        <h5 className="instructor">{props.instructor}</h5>
-        <div className="rating">
+        <h5 className={CourseCardCSS.instructor}>{props.instructor}</h5>
+        <div className={CourseCardCSS.rating}>
             {props.rating}
-            <span className="users">({props.users})</span>
+            <span className={CourseCardCSS.users}>({props.users})</span>
         </div>
         <div>
-            <span className="price">{`E£${props.price}`}</span>
-            <span className="oldPrice">{`E£${props.oldPrice}`}</span>
+            <span className={CourseCardCSS.price}>{`E£${props.price}`}</span>
+            <span className={CourseCardCSS.oldPrice}>{`E£${props.oldPrice}`}</span>
         </div>
         </div>
-    </div>
-
   )
 }
 
