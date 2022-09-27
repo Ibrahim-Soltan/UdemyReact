b17b59e43b4f274c564978da9297c81d8e07a083
Ibrahim Soltan <ibrahim.soltan918@gmail.com>
2022-09-27 12:51:13 +0200

    feat: Adding str method to class Course.

--- server/server/course/models.py
+++ server/server/course/models.py
@@ -1,9 +1,13 @@
+from turtle import title
 from django.db import models
 
 
 class Course (models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(max_length=400)
+
+    def __str__():
+        return title
 
     class Meta:
         db_table = 'courses'
